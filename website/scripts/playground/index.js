53f8ec1871ee0ee1030a02e324c2297245ffa876
Radosław Miernik <radekmie@gmail.com>
2019-05-18 22:17:32 +0200

    Fixed playground.

--- website/scripts/playground/index.js
+++ website/scripts/playground/index.js
@@ -1,10 +1,12 @@
-import BaseField from 'uniforms/BaseField';
 import Frame from 'react-frame-component';
 import React from 'react';
 import ValidatedForm from 'uniforms/ValidatedForm';
 import connectField from 'uniforms/connectField';
+import context from 'uniforms/context';
 import omit from 'lodash/omit';
 import {Component} from 'react';
+import {createContext} from 'react';
+import {useContext} from 'react';
 
 import presets from './presets';
 import schema from './schema';
@@ -14,6 +16,8 @@ import {parseQuery} from './utils';
 import {updateQuery} from './utils';
 
 import './styles.css';
+
+const themeContext = createContext();
 
 class Playground extends Component {
   constructor() {
@@ -62,32 +66,21 @@ class Playground extends Component {
           <PlaygroundPropsField name="props" spellCheck={false} />
         </section>
 
-        <PlaygroundPreviewField name="props" nameTheme="theme" />
+        <PlaygroundPreviewField name="props" />
       </PlaygroundForm>
     );
   }
 }
 
-class PlaygroundField extends BaseField {
-  shouldComponentUpdate(props, state, context) {
-    return (
-      this.context.uniforms.state.theme !== context.uniforms.state.theme ||
-      super.shouldComponentUpdate(props, state, context)
-    );
-  }
-}
-
 class PlaygroundForm extends ValidatedForm {
-  getContextState() {
-    return {
-      ...super.getContextState(),
-      theme: this.props.model.theme
-    };
-  }
-
   render() {
-    const props = omit(this.getNativeFormProps(), ['onSubmit']);
-    return <section {...props} />;
+    return (
+      <themeContext.Provider value={this.props.model.theme}>
+        <context.Provider value={this.getContext()}>
+          <section {...omit(this.getNativeFormProps(), ['onSubmit'])} />
+        </context.Provider>
+      </themeContext.Provider>
+    );
   }
 }
 
@@ -113,8 +106,7 @@ class PlaygroundPreview extends Component {
   }
 
   render() {
-    // FIXME: this.props.theme is undefined during `docusaurus build`.
-    const Form = themes[this.props.theme || 'unstyled'].AutoForm;
+    const Form = themes[this.context].AutoForm;
 
     const {asyncOnSubmit, asyncOnValidate, ...props} = {...this.props.value};
     props.schema = this._schema;
@@ -126,7 +118,7 @@ class PlaygroundPreview extends Component {
     }
 
     return (
-      <PlaygroundWrap theme={this.props.theme}>
+      <PlaygroundWrap theme={this.context}>
         {this.props.errorMessage ? (
           <span children={this.props.errorMessage} />
         ) : (
@@ -140,77 +132,69 @@ class PlaygroundPreview extends Component {
   }
 }
 
-const PlaygroundPreviewField = connectField(PlaygroundPreview, {baseField: PlaygroundField});
+const PlaygroundPreviewField = connectField(PlaygroundPreview);
 
-class PlaygroundProps extends Component {
-  render() {
-    const {onChange, schema, theme, value} = this.props;
+PlaygroundPreview.contextType = themeContext;
 
-    const isAntd = theme === 'antd';
-    const isBootstrap = theme === 'bootstrap3' || theme === 'bootstrap4';
-    const isMaterial = theme === 'material';
-    const isSemantic = theme === 'semantic';
+function PlaygroundProps({onChange, schema, value}) {
+  const theme = useContext(themeContext);
 
-    // FIXME: theme is undefined during `docusaurus build`.
-    const {AutoForm, BoolField, ErrorsField, LongTextField, NumField} = themes[theme || 'unstyled'];
+  const isAntd = theme === 'antd';
+  const isBootstrap = theme === 'bootstrap3' || theme === 'bootstrap4';
+  const isMaterial = theme === 'material';
+  const isSemantic = theme === 'semantic';
 
-    return (
-      <PlaygroundWrap theme={theme}>
-        <AutoForm autosave autosaveDelay={100} model={value} onSubmit={onChange} schema={schema}>
-          <BoolField name="autosave" />
-          <NumField name="autosaveDelay" disabled={!value.autosave} />
-          <BoolField name="disabled" />
-          <BoolField name="label" />
-          <BoolField name="placeholder" />
-          <BoolField name="showInlineError" disabled={!(isAntd || isBootstrap || isMaterial || isSemantic)} />
-          <BoolField name="asyncOnSubmit" />
-          <BoolField name="asyncOnValidate" />
-          <LongTextField name="schema" {...isMaterial && {fullWidth: true, rowsMax: 20}} />
-          <ErrorsField />
-        </AutoForm>
-      </PlaygroundWrap>
-    );
-  }
+  const {AutoForm, BoolField, ErrorsField, LongTextField, NumField} = themes[theme];
+
+  return (
+    <PlaygroundWrap theme={theme}>
+      <AutoForm autosave autosaveDelay={100} model={value} onSubmit={onChange} schema={schema}>
+        <BoolField name="autosave" />
+        <NumField name="autosaveDelay" disabled={!value.autosave} />
+        <BoolField name="disabled" />
+        <BoolField name="label" />
+        <BoolField name="placeholder" />
+        <BoolField name="showInlineError" disabled={!(isAntd || isBootstrap || isMaterial || isSemantic)} />
+        <BoolField name="asyncOnSubmit" />
+        <BoolField name="asyncOnValidate" />
+        <LongTextField name="schema" {...isMaterial && {fullWidth: true, rowsMax: 20}} />
+        <ErrorsField />
+      </AutoForm>
+    </PlaygroundWrap>
+  );
 }
 
-const PlaygroundPropsField = connectField(PlaygroundProps, {baseField: PlaygroundField});
+const PlaygroundPropsField = connectField(PlaygroundProps);
 
-class PlaygroundSelect extends Component {
-  render() {
-    // FIXME: allowedValues is undefined during `docusaurus build`.
-    const {allowedValues = [], onChange, transform, value} = this.props;
-
-    return (
-      <select onChange={event => onChange(event.target.value)} value={value}>
-        {allowedValues.map(value => (
-          <option key={value} value={value}>
-            {transform ? transform(value) : value}
-          </option>
-        ))}
-      </select>
-    );
-  }
+function PlaygroundSelect({allowedValues = [], onChange, transform, value}) {
+  return (
+    <select onChange={event => onChange(event.target.value)} value={value}>
+      {allowedValues.map(value => (
+        <option key={value} value={value}>
+          {transform ? transform(value) : value}
+        </option>
+      ))}
+    </select>
+  );
 }
 
 const PlaygroundSelectField = connectField(PlaygroundSelect);
 
-class PlaygroundWrap extends Component {
-  render() {
-    const {children, theme} = this.props;
-    const content = (
-      <React.Fragment>
-        {children}
-        {styles[theme]}
-      </React.Fragment>
-    );
+function PlaygroundWrap({children}) {
+  const theme = useContext(themeContext);
+  const content = (
+    <React.Fragment>
+      {children}
+      {styles[theme]}
+    </React.Fragment>
+  );
 
-    // MaterialUI injects scoped CSS classes into head.
-    if (theme === 'material') {
-      return <section children={content} className="frame-root playground-wrap" />;
-    }
-
-    return <Frame children={content} />;
+  // MaterialUI injects scoped CSS classes into head.
+  if (theme === 'material') {
+    return <section children={content} className="frame-root playground-wrap" />;
   }
+
+  return <Frame children={content} />;
 }
 
 export default Playground;
